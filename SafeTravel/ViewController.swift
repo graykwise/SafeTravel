de8ea3e16df40308502fd4a0cd66904e38829713
Grayson Wise <graysonwise3@gmail.com>
2017-06-01 21:01:57 +0200

    uploaded finished app prototype

---
+++ SafeTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafeTravel
+//
+//  Created by Grayson Wise on 6/1/17.
+//  Copyright © 2017 Grayson Wise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
